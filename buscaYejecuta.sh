98a6d22e68fe1502fc1af467f062ffa235d32b99
Manuel Romero <mrm8488@gmail.com>
2017-06-28 21:58:53 +0200

    Fix several typos

--- buscaYejecuta.sh
+++ buscaYejecuta.sh
@@ -2,7 +2,7 @@
 
 # create by mrm8488@gmail.com
 
-echo "Insert the command (options icludede) you remember to have executed:"
+echo "Insert the command (options icluded) you remember to have executed:"
 read input
 cd $HOME
 IFS=$'\n'
@@ -11,14 +11,14 @@ unset IFS
 
 if [ ${#options[@]} -eq 0 ]
 then
- echo "There is no matches in your bash history for: $input"
+ echo "There are no matches in your bash history for: $input"
  exit 0
 else
- echo "Choose the command you want to execute:"
+ echo "Choose the command (number) you want to execute:"
 
  let arrayLength=${#options[@]}-1
 
-let index=0;
+ let index=0;
  
  for i in `seq 0 1 $arrayLength`
  do
@@ -26,11 +26,11 @@ let index=0;
    echo "$index) ${options[$i]}"
  done
 
-read option
+ read option
 
-let option=$option-1
+ let option=$option-1
 
-${options[$option]}
+ ${options[$option]}
 
-fi
+ fi
 exit 0
